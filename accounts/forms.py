190b454b88a10fdbdf904d250346b038ff9c3a3c
Níkolas Vargas <vargasnikolass@gmail.com>
2019-03-10 00:09:10 -0300

    form data for create new category

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm
-from accounts.models import Transaction
+from accounts.models import Transaction, Category
 
 
 class TransactionForm(ModelForm):
     class Meta:
         model = Transaction
         fields = ['description', 'value', 'category', 'note']
+
+
+class CategoryForm(ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
