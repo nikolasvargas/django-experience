3f25d9f7606a2bbc4c21a62fe0d4fd8d0bae3b78
Níkolas Vargas <vargasnikolass@gmail.com>
2019-03-10 00:11:02 -0300

    some tests with djando.test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.test.client import Client
+from accounts.models import Category
 
-# Create your tests here.
+
+class ViewTest(TestCase):
+
+    def test_home_status_code_and_template_used(self):
+        client = Client()
+        response = client.get('/home/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/home.html')
+
+    def test_form_cant_register_new_transaction(self):
+        pass
